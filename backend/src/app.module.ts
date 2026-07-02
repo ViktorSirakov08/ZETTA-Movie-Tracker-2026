0977f12a9d25f53230c5894f04d46c2a5848e6b8
Viktor Sirakov <133885537+ViktorSirakov08@users.noreply.github.com>
2026-07-02 14:11:02 +0300

    resolved media issues (media module got lost somewhere idk)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MediaModule } from './media/media.module';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -27,30 +28,8 @@ import { AuthModule } from './auth/auth.module';
     }),
     UsersModule,
     AuthModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    MediaModule,
+  ]
 })
 export class AppModule {}
-import { MediaModule } from './media/media.module';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: parseInt(config.get<string>('DB_PORT') || '5432', 10), // Cast explicitly to a number
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-        }),
-    }),
-    MediaModule,
-  ],
-})
